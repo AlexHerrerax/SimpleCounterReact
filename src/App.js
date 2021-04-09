5438ef39b9047890cd68c774e845765ca68f7582
Alex <axherrerax@gmail.com>
2021-04-09 02:34:27 -0400

    contador actualizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="contenedor">
+      <div className="icono"> <i className="fas fa-clock"></i></div>
+      <h1 id="cuatro">{props.four%10}</h1>
+      <h1 id="tres">{props.three%10}</h1>
+      <h1 id="dos">{props.two%10}</h1>
+      <h1 id="uno">{props.one%10}</h1>
+
     </div>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { getAllByAltText } from '@testing-library/dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
+
+
+let contador=0
+
+
+
+setInterval( function () {
+    let a=Math.floor(contador);
+let b=Math.floor(contador/10);
+let c=Math.floor(contador/100);
+let d=Math.floor(contador/1000);
+    contador++;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<App 
+one={a}
+two={b}
+three={c}
+four={d}
+ />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}, 1000); 
+
